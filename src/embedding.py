136734926048edaf87c16b789bf4bf254749d0bb
Himanshu <khartadehimanshu@gmail.com>
2026-07-01 23:16:09 +0530

    Refactor data collection and embedding generation into src modules

--- src/embedding.py
+++ src/embedding.py
@@ -2,3 +2,20 @@ import pandas as pd
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
+def load_embedding_model(modal_name = "all-MiniLM-L6-v2"):
+    modal = SentenceTransformer(modal_name)
+    return modal
+
+def generate_embedding(df,model):
+    documents = (
+        df["Title"].fillna("") +
+        " " +
+        df["Abstract"].fillna("")
+    ).tolist()
+
+    embeddings = model.encode(
+        documents,
+        show_progress_bar=True
+    )
+
+    return embeddings
